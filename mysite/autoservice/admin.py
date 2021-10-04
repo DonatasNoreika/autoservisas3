876bcba2c3bc76f4d5f648b5f2c00dc5138d1596
Donoras <donoras@gmail.com>
2021-10-04 22:09:58 +0300

    +Sukurti naują Django projektą su appsu Autoservice +Sukurti visus modelius pagal nurodytą programos DB struktūrą +Sukurti meniu punktus visiems sukurtiems modeliams +Susikurti superuser vartotoją, prisijungti ir išbandyti įrašyti visų modelių įrašus

--- mysite/autoservice/admin.py
+++ mysite/autoservice/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Service, CarModel, Car, Order, OrderLine
+
+admin.site.register(Service)
+admin.site.register(CarModel)
+admin.site.register(Car)
+admin.site.register(Order)
+admin.site.register(OrderLine)
