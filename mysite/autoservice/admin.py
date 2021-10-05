7e5cc0e17ed1b10ce5972464ace6b89c1a63e5b7
Donoras <donoras@gmail.com>
2021-10-05 19:20:28 +0300

    Padaryti, kad modelių pavadinimai būtų atvaizduojami teisingai (vienaskaita ir daugiskaita) Padaryti, kad užsakymo formoje būtų matomos ir užsakymo eilutės (į jas galima būtų įrašyti informaciją)

--- mysite/autoservice/admin.py
+++ mysite/autoservice/admin.py
@@ -3,8 +3,15 @@ from django.contrib import admin
 # Register your models here.
 from .models import Service, CarModel, Car, Order, OrderLine
 
+class OrderLineInline(admin.TabularInline):
+    model = OrderLine
+    extra = 0 # išjungia placeholder'ius
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [OrderLineInline]
+
 admin.site.register(Service)
 admin.site.register(CarModel)
 admin.site.register(Car)
-admin.site.register(Order)
+admin.site.register(Order, OrderAdmin)
 admin.site.register(OrderLine)
